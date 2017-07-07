f6a94f1d0ac40c6bec04ea428f4bc356cdd594b6
Kathryn Smith <smithkb@uchicago.edu>
2017-07-07 09:55:45 -0500

    Add more pages

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -7,4 +7,16 @@ class StaticPagesController < ApplicationController
 
   def about
   end
+
+  def tourism
+  end
+
+  def registry
+  end
+
+  def wedding_party
+  end
+
+  def our_family
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,12 @@ Rails.application.routes.draw do
   root 'static_pages#home'
   get '/about', to: 'static_pages#about'
   get '/wedding_party', to: 'static_pages#wedding_party'
-  get '/tourism', to:'static_pages#tourism'
+  get '/hotels', to:'static_pages#hotel_blocks'
+  get '/hindu_weddings', to:'static_pages#hindu_weddings'
+  get '/registry', to: 'static_pages#registry'
+  get '/tourism', to:'static_pages#things_to_do'
   get '/schedule_of_events', to: 'static_pages#schedule_of_events'
+  get '/our_family', to: 'static_pages#our_family'
   get '/information', to: 'static_pages#information'
 
 end
