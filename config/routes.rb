05975d1354f62b6f22a48f1f3f59690767648be6
Kathryn Smith <smithkb@uchicago.edu>
2017-07-07 11:58:20 -0500

    Styling and more content

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
+  get '/home', to: 'static_pages#home'
   get '/about', to: 'static_pages#about'
   get '/wedding_party', to: 'static_pages#wedding_party'
   get '/hotels', to:'static_pages#hotel_blocks'
